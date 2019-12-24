f23e2cba300ddac50374066f5fc7b2d62365d2da
Roman Volosatovs <rvolosatovs@riseup.net>
2019-12-24 16:10:22 +0100

    provision.sh: Support Wirnet iStation

--- provision.sh
+++ provision.sh
@@ -10,9 +10,14 @@ set -e -o pipefail
 uplinkPort=1700
 downlinkPort=1700
 
-userDir="/mnt/fsuser-1"
-loradDir="${userDir}/lorad"
-lorafwdDir="${userDir}/lorafwd"
+wirnetStationUserDir="/mnt/fsuser-1"
+wirnetIStationUserDir="/user/etc"
+loradConfigName="lorad.json"
+lorafwdConfigName="lorafwd.toml"
+
+wirnetStationModel="wirnet-station"
+wirnetIStationModel="wirnet-istation"
+
 dotaName="dota.tar.gz"
 
 updateAndRetryFmt="please update gateway firmware to at least version '%s' and rerun the script"
@@ -23,8 +28,8 @@ function sshExec {
     ${1} "sh -l -c '${2}'"
 }
 
-# firmwareVersion uses ${1} to get version and stores it in ${2}.
-function firmwareVersion {
+# wirnetStationFirmwareVersion uses ${1} to get version and stores it in ${2}.
+function wirnetStationFirmwareVersion {
     local fsVer
     fsVer=$(sshExec "${1}" 'get_version -u' | grep 'FILESYSTEM_VER' | cut -d '=' -f 2)
 
@@ -76,17 +81,17 @@ function isUp {
     ping -q -c 1 "${gatewayAddr}" > /dev/null 
 }
 
-# pushFirmware downloads ${2}, checks that it's sha256sum matches ${3} and if it does, pushes it to root@${1}:${userDir}/dota/dota.tar.gz via `scp`, performs a reboot of ${1} via `ssh` and waits until ${1} boots up. 
-function pushFirmware {
-    local gatewayAddr=${1}
-    local url=${2}
-    local sha256=${3}
+# pushWirnetStationFirmware downloads ${2}, checks that it's sha256sum matches ${3} and if it does, pushes it to root@${1}:${wirnetStationUserDir}/dota/dota.tar.gz via `scp`, performs a reboot of ${1} via `ssh` and waits until ${1} boots up. 
+function pushWirnetStationFirmware {
+    local gatewayAddr="${1}"
+    local url="${2}"
+    local sha256="${3}"
 
     local tmpDir
     tmpDir="$(mktemp -d)"
     pushd "${tmpDir}" > /dev/null
 
-    local gatewayPath="${userDir}/dota/${dotaName}"
+    local gatewayPath="${wirnetStationUserDir}/dota/${dotaName}"
     local localPath="${tmpDir}/${dotaName}"
 
     local err
@@ -176,10 +181,10 @@ function pushFirmware {
 
 # pushConfig downloads ${2} and pushes it to root@${1}:${3} via `scp`. 
 function pushConfig {
-    local gatewayAddr=${1}
-    local apiKey=${2}
-    local url=${3}
-    local gatewayPath=${4}
+    local gatewayAddr="${1}"
+    local apiKey="${2}"
+    local url="${3}"
+    local gatewayPath="${4}"
 
     local tmpDir
     tmpDir="$(mktemp -d)"
@@ -232,9 +237,11 @@ function pushConfig {
 }
 
 function isCPFInstalled {
-    local -n ret=$1
+    local loradConfig="${1}"
+    local lorafwdConfig="${2}"
+    local -n ret=$3
     set +e
-    err=$(${sshCmd} "test -d ${loradDir}/etc && test -d ${lorafwdDir}/etc" 2>&1)
+    err=$(${sshCmd} "test -f ${loradConfig} && test -f ${lorafwdConfig}" 2>&1)
     if [[ $? -ne 0 ]]; then
         if [[ -n "${err}" ]]; then
             printf "Testing if CPF is installed on gateway failed:\n" >&2
@@ -252,114 +259,169 @@ function isCPFInstalled {
 }
 
 function provision {
-    local gatewayAddr="${1}"
-    local stackAddr="${2}"
-    local gatewayID="${3}"
-    local apiKey="${4}"
+    local gatewayModel="${1}"
+    local gatewayAddr="${2}"
+    local stackAddr="${3}"
+    local gatewayID="${4}"
+    local apiKey="${5}"
+
+    local userDir
+    local loradConfig
+    local lorafwdConfig
+    case "${gatewayModel}" in
+        "${wirnetStationModel}")
+            userDir="${wirnetStationUserDir}"
+            loradConfig="${userDir}/lorad/etc/lorad.json"
+            lorafwdConfig="${userDir}/lorafwd/etc/lorafwd.toml"
+            ;;
+
+        "${wirnetIStationModel}")
+            userDir="${wirnetIStationUserDir}"
+            loradConfig="${userDir}/lorad/lorad.json"
+            lorafwdConfig="${userDir}/lorafwd/lorafwd.toml"
+            ;;
+
+        *)
+            printf "Unsupported gateway model '${gatewayModel}'.\nSupported gateway models are: '${wirnetStationModel}', '${wirnetIStationModel}'.\n" >&2
+            return 1
+    esac
 
     local sshCmd="ssh ${sshParams} root@${gatewayAddr}"
 
     local -i cpfInstalled
-    isCPFInstalled cpfInstalled
+    isCPFInstalled "${loradConfig}" "${lorafwdConfig}" cpfInstalled
     if [[ ${cpfInstalled} -eq 0 ]]; then
-        local ans
-        read -r -n 1 -p "CPF is not installed, attempt CPF '1.1.6' installation?[y/n]" ans
-        printf '\n' >&2
-        if [[ ! "${ans}" = "y" ]]; then
-            printf "CPF installation aborted, please install CPF and rerun the script\n" >&2
-            return 1
-        fi
-
-        local fwVer
-        firmwareVersion "${sshCmd}" fwVer
-
-        case "${fwVer}" in
-            [0-2].* )
-                printf "Gateway firmware version '${fwVer}' is too low, ${updateAndRetryFmt}\n" "3.0" >&2
-                return 1
-                ;;
-            3.[0-5] | 3.[0-5].* )
-                read -r -n 1 -p "Gateway firmware version is '${fwVer}', which is lower than '3.6' required by CPF, attempt firmware update to '3.6'?[y/n]" ans
+        case "${gatewayModel}" in
+            "${wirnetStationModel}")
+                local ans
+                read -r -n 1 -p "CPF is not installed, attempt CPF '1.1.6' installation?[y/n]" ans
                 printf '\n' >&2
                 if [[ ! "${ans}" = "y" ]]; then
-                    printf "Firmware update aborted, ${updateAndRetryFmt}\n" '3.6' >&2
+                    printf "CPF installation aborted, please install CPF and rerun the script\n" >&2
                     return 1
                 fi
 
-                pushFirmware    "${gatewayAddr}" \
-                                "https://github.com/TheThingsNetwork/kerlink-station-firmware/releases/download/wirnet-3.6/fwupgrade_wirmav2_wirnet_v3.6.tar.gz" \
-                                "63c606af73f983fcb9122086099acf01c5498ff92a06bc76a78b6e0bcdf269ba"
-                
-                ssh-keygen -R "${gatewayAddr}"
+                local fwVer
+                wirnetStationFirmwareVersion "${sshCmd}" fwVer
 
-                firmwareVersion "${sshCmd}" fwVer
-                if [[ ! "${fwVer}" = 3.6 ]]; then
-                    printf "Firmware update failed, gateway firmware version is '${fwVer}', ${updateAndRetryFmt}\n" "3.6" >&2
+                case "${fwVer}" in
+                    [0-2].*)
+                        printf "Gateway firmware version '${fwVer}' is too low, ${updateAndRetryFmt}\n" "3.0" >&2
+                        return 1
+                        ;;
+
+                    3.[0-5] | 3.[0-5].*)
+                        read -r -n 1 -p "Gateway firmware version is '${fwVer}', which is lower than '3.6' required by CPF, attempt firmware update to '3.6'?[y/n]" ans
+                        printf '\n' >&2
+                        if [[ ! "${ans}" = "y" ]]; then
+                            printf "Firmware update aborted, ${updateAndRetryFmt}\n" '3.6' >&2
+                            return 1
+                        fi
+
+                        pushWirnetStationFirmware   "${gatewayAddr}" \
+                                                    "${userDir}" \
+                                                    "https://github.com/TheThingsNetwork/kerlink-station-firmware/releases/download/wirnet-3.6/fwupgrade_wirmav2_wirnet_v3.6.tar.gz" \
+                                                    "63c606af73f983fcb9122086099acf01c5498ff92a06bc76a78b6e0bcdf269ba"
+                        
+                        ssh-keygen -R "${gatewayAddr}"
+
+                        wirnetStationFirmwareVersion "${sshCmd}" fwVer
+                        if [[ ! "${fwVer}" = 3.6 ]]; then
+                            printf "Firmware update failed, gateway firmware version is '${fwVer}', ${updateAndRetryFmt}\n" "3.6" >&2
+                            return 1
+                        fi
+
+                        pushWirnetStationFirmware   "${gatewayAddr}" \
+                                                    "${userDir}" \
+                                                    "https://github.com/TheThingsNetwork/kerlink-station-firmware/releases/download/wirnet-3.6/custo_knetd-4.12.tar.gz" \
+                                                    "503aa5336ed5ee3b674a682dfae4f3964038b00e7380e3ee7b1505b01a9678cb"
+                        ;;
+                esac
+
+                pushWirnetStationFirmware   "${gatewayAddr}" \
+                                            "${userDir}" \
+                                            "https://github.com/TheThingsNetwork/kerlink-station-firmware/releases/download/cpf-1.1.6/dota_cpf_1.1.6-1.tar.gz" \
+                                            "df442b0dbfffe1cb878ae1471c44498454e99fc09044f08e1c51941a27e08f8c"
+
+                isCPFInstalled "${loradConfig}" "${lorafwdConfig}" cpfInstalled
+                if [[ ${cpfInstalled} -eq 0 ]]; then
+                    printf "CPF installation failed, please install CPF and rerun the script\n" >&2
                     return 1
                 fi
+                ;;
 
-                pushFirmware    "${gatewayAddr}" \
-                                "https://github.com/TheThingsNetwork/kerlink-station-firmware/releases/download/wirnet-3.6/custo_knetd-4.12.tar.gz" \
-                                "503aa5336ed5ee3b674a682dfae4f3964038b00e7380e3ee7b1505b01a9678cb"
+            *)
+                printf "CPF is not installed, please install CPF and rerun the script\n" >&2
+                return 1
                 ;;
         esac
-
-        pushFirmware    "${gatewayAddr}" \
-                        "https://github.com/TheThingsNetwork/kerlink-station-firmware/releases/download/cpf-1.1.6/dota_cpf_1.1.6-1.tar.gz" \
-                        "df442b0dbfffe1cb878ae1471c44498454e99fc09044f08e1c51941a27e08f8c"
-
-        isCPFInstalled cpfInstalled
-        if [[ ${cpfInstalled} -eq 0 ]]; then
-            printf "CPF installation failed, please install CPF and rerun the script\n" >&2
-            return 1
-        fi
     fi
 
     pushConfig  "${gatewayAddr}" \
                 "${apiKey}" \
                 "${stackAddr}/api/v3/gcs/gateways/${gatewayID}/kerlink-cpf/lorad/lorad.json" \
-                "${loradDir}/etc/lorad.json"
+                "${loradConfig}"
 
     pushConfig  "${gatewayAddr}" \
                 "${apiKey}" \
                 "${stackAddr}/api/v3/gcs/gateways/${gatewayID}/kerlink-cpf/lorafwd/lorafwd.toml" \
-                "${lorafwdDir}/etc/lorafwd.toml"
+                "${lorafwdConfig}"
+
 
     read -r -n 1 -p "Provisioning successfully finished, restart packet forwader?[y/n]" ans
     printf '\n' >&2
     if [[ ! "${ans}" = "y" ]]; then
-        printf "Please manually restart the packet forwarder by executing \`ssh root@${gatewayAddr} /etc/init.d/knet restart\`\n" >&2
+        printf "Please manually restart the packet forwarder\n" >&2
         return 0
     fi
+
     printf "Restarting packet forwarder...\n" >&2
-    ${sshCmd} "/etc/init.d/knet restart"
+    case "${gatewayModel}" in
+        "${wirnetStationModel}")
+            ${sshCmd} "/etc/init.d/knet restart"
+            ;;
+
+        "${wirnetIStationModel}")
+            ${sshCmd} "/etc/init.d/lorad restart"
+            ${sshCmd} "/etc/init.d/lorafwd restart"
+            ;;
+
+        *)
+            printf "Restarting '${gatewayModel}' is not supported, please manually restart the packet forwarder" >&2
+            ;;
+    esac
+
     return 0
 }
 
 function printUsage {
-    printf "Usage: $0 gateway-address stack-address gateway-id api-key\n" >&2
+    printf "Usage: $0 gateway-model gateway-address stack-address gateway-id api-key\n" >&2
 }
 
-if [[ $# -ne 4 ]] ; then
-    printf "${0} takes exactly 4 arguments, got %d\n" $# >&2
+if [[ $# -ne 5 ]]; then
+    printf "${0} takes exactly 5 arguments, got %d\n" $# >&2
     printUsage
     exit 1
 elif [[ -z ${1} ]]; then
-    printf 'Gateway address must be set\n' >&2
+    printf 'Gateway model must be set\n' >&2
     printUsage
     exit 1
 elif [[ -z ${2} ]]; then
-    printf 'Stack address must be set\n' >&2
+    printf 'Gateway address must be set\n' >&2
     printUsage
     exit 1
 elif [[ -z ${3} ]]; then
-    printf 'Gateway ID must be set\n' >&2
+    printf 'Stack address must be set\n' >&2
     printUsage
     exit 1
 elif [[ -z ${4} ]]; then
+    printf 'Gateway ID must be set\n' >&2
+    printUsage
+    exit 1
+elif [[ -z ${5} ]]; then
     printf 'API key must be set\n' >&2
     printUsage
     exit 1
 fi
 
-provision "${1}" "${2}" "${3}" "${4}"
+provision "${1}" "${2}" "${3}" "${4}" "${5}"
